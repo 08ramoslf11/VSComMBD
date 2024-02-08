22d2933d4e444daf20b9e3298ec51e0773389b07
08ramoslf11 <157762398+08ramoslf11@users.noreply.github.com>
2024-02-07 22:30:01 -0300

    fiz

---
+++ Migrations/20240208012719_FimBD.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VSComMDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class FimBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", maxLength: 100, nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Celular = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CEP = table.Column<string>(type: "varchar(9)", maxLength: 9, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
---
+++ Model/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VSComMDB.Model
+{
+    public class Pessoa
+    {
+
+        [MaxLength(100)]    
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [MaxLength(15)]
+        public string? Celular { get; set; }
+
+        [MaxLength(9)]
+        public string CEP { get; set;}
+    }
+}
